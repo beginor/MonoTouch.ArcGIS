573691d4819bbe38873c03bd82dc7208be1468e8
beginor <beginor@qq.com>
2012-08-05 16:43:50 +0800

    add solution project file

---
+++ Binding/Api.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Drawing;
+
+using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MonoTouch.ArcGIS {
+	// For more information, see http://docs.xamarin.com/ios/advanced_topics/binding_objective-c_types
+	//
+
+}
+
