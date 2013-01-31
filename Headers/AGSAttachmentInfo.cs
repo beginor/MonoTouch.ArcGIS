581e00a7a57178140ec559ed6d9fc910399a1bf9
beginor <beginor@qq.com>
2013-01-31 13:45:44 +0800

    binding AGSAttachmentInfo

---
+++ Headers/AGSAttachmentInfo.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Drawing;
+
+using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MonoTouch.ArcGIS {
+
+	[BaseType(typeof(NSObject))]
+	public interface AGSAttachmentInfo : AGSCoding {
+
+		[Export("attachmentId", ArgumentSemantic.Assign)]
+		int AttachmentId { get; }
+
+		[Export("contentType", ArgumentSemantic.Copy)]
+		string ContentType { get; }
+
+		[Export("size", ArgumentSemantic.Assign)]
+		int Size { get; }
+
+		[Export("name", ArgumentSemantic.Copy)]
+		string Name { get; }
+
+		[Export("isEqualToAttachmentInfo:")]
+		bool IsEqualToAttachmentInfo(AGSAttachmentInfo other);
+	}
+}
