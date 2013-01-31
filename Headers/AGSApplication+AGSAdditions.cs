1ad9ab654905d4fd6dc0ea2f8a6da968b54f8ae2
beginor <beginor@qq.com>
2013-01-31 13:09:50 +0800

    binding AGSApplication+AGSAdditions.h

--- Headers/AGSApplication+AGSAdditions.cs
+++ Headers/AGSApplication+AGSAdditions.cs
@@ -7,4 +7,25 @@ using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
 namespace MonoTouch.ArcGIS {
+
+	[BaseType(typeof(UIApplication))]
+	public interface AGSApplication {
+
+		[Export("ags_setNetworkActivityDelegate:")]
+		void SetNetworkActivityDelegate(AGSNetworkActivityDelegate agsNetworkActivityDelegate);
+
+		[Export("ags_showNetworkActivityIndicator:")]
+		void ShowNetworkActivityIndicator(bool show);
+	}
+
+	[Model, BaseType(typeof(NSObject))]
+	public interface AGSNetworkActivityDelegate {
+
+		[Export("networkActivityInProgress")]
+		void NetworkActivityInProgress();
+
+		[Export("networkActivityEnded")]
+		void NetworkActivityEnded();
+		
+	}
 }
