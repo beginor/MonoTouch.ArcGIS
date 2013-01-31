bf8465c92666f6dd95d6a150addf96f83d99aa66
beginor <beginor@qq.com>
2013-01-31 13:18:35 +0800

    binding AGSAreasAndLengthsParameters

---
+++ Headers/AGSAreasAndLengthsParameters.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Drawing;
+
+using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MonoTouch.ArcGIS {
+
+	[BaseType(typeof(NSObject))]
+	public interface AGSAreasAndLengthsParameters : AGSCoding {
+
+		[Export("polygons", ArgumentSemantic.Copy)]
+		AGSPolygon[] Polygons { get; set; }
+
+		[Export("lengthUnit", ArgumentSemantic.Assign)]
+		AGSSRUnit LengthUnit { get; set; }
+
+		[Export("areaUnit"), ArgumentSemantic.Assign]
+		AGSAreaUnits AreaUnit { get; set; }
+
+		[Export("areasAndLengthsParameters", Static)]
+		AGSAreasAndLengthsParameters AreasAndLengthsParameters();
+	}
+}
