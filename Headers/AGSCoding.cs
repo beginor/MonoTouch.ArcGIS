2581abef87fa56131159be18c3c4a5e5e884b5d6
beginor <beginor@qq.com>
2013-01-31 10:24:27 +0800

    bind AGSCoding.h

---
+++ Headers/AGSCoding.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Drawing;
+
+using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MonoTouch.ArcGIS {
+
+	/// <summary>A protocol to convert objects to and from JSON.</summary>
+	[Model, BaseType(typeof(NSObject))]
+	public interface AGSCoding {
+
+		/// <summary>Initialize and return object from JSON representation.</summary>
+		[Export("initWithJSON:")]
+		IntPtr Constructor(Dictionary json);
+
+		/// <summary>Decode object from JSON representation.</summary>
+		[Export("decodeWithJSON:")]
+		void DecodeWithJson(Dictionary json);
+
+		/// <summary>Encode and return JSON representation for object.</summary>
+		[Export("encodeToJSON")]
+		Dictionary EncodeToJson();
+	}
+}
