7671e40d694a50fc088cda003ea271b7bbecc30b
beginor <beginor@qq.com>
2013-01-31 13:37:13 +0800

    add AGSAttachment binding

---
+++ Headers/AGSAttachment.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Drawing;
+
+using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MonoTouch.ArcGIS {
+
+	[BaseType(typeof(NSObject))]
+	public interface AGSAttachment {
+
+		[Export("attachmentInfo", ArgumentSemantic.Strong)]
+		AGSAttachmentInfo AttachmentInfo { get; }
+
+		[Export("filepath", ArgumentSemantic.Copy)]
+		string FilePath { get; set; }
+
+		[Export("editState", ArgumentSemantic.Assign)]	
+		AGSAttachmentEditState EditState { get; }
+
+		[Export("editResultError", ArgumentSemantic.Strong)]
+		AGSEditResultError EditResultError { get; }
+
+		[Export("networkError", ArgumentSemantic.Strong)]
+		NSError NetworkError { get; }
+
+		[Export("isDownloading", ArgumentSemantic.Assign)]
+		bool IsDownloading { get; }
+
+		[Export("markForDeletion:")]
+		void MarkForDeletion(bool del);
+
+		[Export("existsOnServer")]
+		bool ExistsOnServer();
+
+		[Export("isLocal")]
+		bool IsLocal();
+
+		[Export("thumbnail")]
+		AGSImage GetThumbnail();
+
+		[Export("data")]
+		NSData GetData();
+	}
+}
