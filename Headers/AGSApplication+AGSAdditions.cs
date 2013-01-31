7b29d708c0b1fa1847c2611995a8dfc3dc55cd07
beginor <beginor@qq.com>
2013-01-31 10:26:05 +0800

    add AGSApplication + AGSAddtions.cs

---
+++ Headers/AGSApplication+AGSAdditions.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Drawing;
+
+using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MonoTouch.ArcGIS {
+}
