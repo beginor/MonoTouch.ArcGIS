3cef8ffa0d816b66015b1777e6ad958f1245173c
beginor <beginor@qq.com>
2013-02-07 07:30:31 +0800

    add binding files

--- Headers/enums.cs
+++ Headers/enums.cs
@@ -5,3 +5,18 @@ public enum AGSAttachmentEditState {
 	MarkedForDeletion,
 	DeletedFromServer 
 }
+
+public enum AGSBingMapLayerStyle {
+	Aerial,
+	AerialWithLabels,
+	Road
+}
+
+[Flag]
+public enum AGSCalloutLeaderPosition {
+	Bottom = 1UL << 0,
+	Top = 1UL << 1,
+	Left = 1UL << 2,
+	Right = 1UL << 3,
+	Any = Bottom | Top | Left | Right
+}
