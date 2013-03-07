ba232fb098a4dc36425f3e57a49eeef225f915fd
beginor <beginor@qq.com>
2013-03-08 07:32:34 +0800

    property converter

--- Parser/ObjcPropertyConverter.cs
+++ Parser/ObjcPropertyConverter.cs
@@ -5,13 +5,30 @@ namespace Parser {
 
 	public class ObjcPropertyConverter : ObjcConverter {
 
-		public ObjcPropertyConverter(ObjcPropertyToken token) : base(token) {}
+		public ObjcPropertyConverter(ObjcPropertyToken token) : base(token) { }
 
 		public override CodeObject Convert() {
-			CodeMemberProperty property = new CodeMemberProperty();
+			var propToken = (ObjcPropertyToken)this.Token;
+			var property = new CodeMemberProperty {
+				Name = ToCsharpPropertyName(propToken.PropertyName),
+				Type = new CodeTypeReference(propToken.PropertyType),
+				HasGet = true,
+				HasSet = !propToken.IsReadOnly
+			};
+
+			var exportAttr = new CodeAttributeDeclaration("Export");
+			exportAttr.Arguments.Add(new CodeAttributeArgument(new CodePrimitiveExpression(propToken.PropertyName)));
+			exportAttr.Arguments.Add(new CodeAttributeArgument(new CodeSnippetExpression("ArgumentSemantic." + ToCsharpPropertyName(propToken.Semantic))));
+			property.CustomAttributes.Add(exportAttr);
+			// getter and setter export?
+			//property.GetStatements.Add()
+			property.Comments.Add(new CodeCommentStatement(propToken.Code));
 			return property;
 		}
 
+		static string ToCsharpPropertyName(string objcPropName) {
+			return objcPropName.Substring(0, 1).ToUpperInvariant() + objcPropName.Substring(1);
+		}
 	}
 
 }
---
+++ Parser/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Parser {
+	
+	class StringExtensions {
+		
+
+	}
+}
