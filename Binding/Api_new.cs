4e384c3e7b56cedf314d08c0258274d3c462fd5e
beginor <beginor@qq.com>
2013-07-21 16:48:11 +0800

    since has lot of error when compile api file directly, try another method, create a new api file, add needed feature, try always keep project can be compiled

---
+++ Binding/Api_new.cs
@@ -0,0 +1,254 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.CoreLocation;
+using MonoTouch.Security;
+
+using OSStatus = System.Int32;
+
+namespace MonoTouch.ArcGIS {
+
+	[Model, BaseType (typeof (NSObject))]
+	public partial interface AGSCoding {
+
+		[Export ("initWithJSON:")]
+		IntPtr Constructor (NSDictionary json);
+
+		[Export ("decodeWithJSON:")]
+		void DecodeWithJSON (NSDictionary json);
+
+		[Export ("encodeToJSON")]
+		NSDictionary EncodeToJSON();
+	}
+
+	[BaseType (typeof (NSObject))]
+	public partial interface AGSSpatialReference : AGSCoding {
+
+		[Export ("wkid")]
+		uint Wkid { get; }
+
+		[Export ("wkt", ArgumentSemantic.Copy)]
+		string Wkt { get; }
+
+		[Export ("initWithWKID:")]
+		IntPtr Constructor (uint wkid);
+
+		[Export ("initWithWKT:")]
+		IntPtr Constructor (string wkt);
+
+		[Static, Export ("spatialReferenceWithWKID:")]
+		NSObject SpatialReferenceWithWKID (uint wkid);
+
+		[Static, Export ("spatialReferenceWithWKT:")]
+		NSObject SpatialReferenceWithWKT (string wkt);
+
+		[Export ("initWithWKID:WKT:")]
+		IntPtr Constructor (uint wkid, string wkt);
+
+		[Static, Export ("spatialReferenceWithWKID:WKT:")]
+		NSObject SpatialReferenceWithWKID (uint wkid, string wkt);
+
+		[Export ("isAnyWebMercator")]
+		bool IsAnyWebMercator ();
+
+		[Export ("isWGS84")]
+		bool IsWGS84 ();
+
+		[Export ("isEqualToSpatialReference:")]
+		bool IsEqualToSpatialReference (AGSSpatialReference sr);
+
+		[Export ("isEqualToWKID:")]
+		bool IsEqualToWKID (uint wkid);
+
+		[Export ("encodeToJSON:forKey:")]
+		void EncodeToJSON (NSMutableDictionary json, string key);
+
+		[Export ("unit")]
+		AGSSRUnit Unit ();
+
+		[Static, Export ("webMercatorSpatialReference")]
+		AGSSpatialReference WebMercatorSpatialReference ();
+
+		[Static, Export ("wgs84SpatialReference")]
+		AGSSpatialReference Wgs84SpatialReference ();
+
+		[Export ("convertValue:toUnit:")]
+		double ConvertValueToUnit (double val, AGSSRUnit unit);
+
+		[Export ("convertValue:fromUnit:")]
+		double ConvertValueFromUnit (double val, AGSSRUnit fromUnit);
+
+		[Export ("isSupported")]
+		bool IsSupported ();
+	}
+
+	[BaseType (typeof (NSObject))]
+	public partial interface AGSGeometry : AGSCoding {
+
+		[Export ("spatialReference", ArgumentSemantic.Retain)]
+		AGSSpatialReference SpatialReference { get; }
+
+		[Export ("envelope", ArgumentSemantic.Copy)]
+		AGSEnvelope Envelope { get; }
+
+		[Export ("initWithSpatialReference:")]
+		IntPtr Constructor (AGSSpatialReference spatialReference);
+
+		[Export ("initWithJSON:spatialReference:")]
+		IntPtr Constructor (NSDictionary json, AGSSpatialReference sr);
+
+		[Export ("intersectsWithEnvelope:")]
+		bool IntersectsWithEnvelope (AGSEnvelope envelope);
+
+		[Export ("isEmpty")]
+		bool IsEmpty ();
+
+		[Export ("isValid")]
+		bool IsValid ();
+	}
+
+	[BaseType (typeof (AGSGeometry))]
+	public partial interface AGSEnvelope {
+
+		[Export ("xmin")]
+		double Xmin { get; }
+
+		[Export ("ymin")]
+		double Ymin { get; }
+
+		[Export ("xmax")]
+		double Xmax { get; }
+
+		[Export ("ymax")]
+		double Ymax { get; }
+
+		[Export ("center", ArgumentSemantic.Retain)]
+		AGSPoint Center { get; }
+
+		[Export ("width")]
+		double Width { get; }
+
+		[Export ("height")]
+		double Height { get; }
+
+		[Export ("initWithXmin:ymin:xmax:ymax:spatialReference:")]
+		IntPtr Constructor (double xmin, double ymin, double xmax, double ymax, AGSSpatialReference spatialReference);
+
+		[Static, Export ("envelopeWithXmin:ymin:xmax:ymax:spatialReference:")]
+		AGSEnvelope EnvelopeWithXmin (double xmin, double ymin, double xmax, double ymax, AGSSpatialReference spatialReference);
+
+		[Export ("containsPoint:")]
+		bool ContainsPoint (AGSPoint point);
+
+		[Export ("containsEnvelope:")]
+		bool ContainsEnvelope (AGSEnvelope env);
+
+		[Export ("simpleRepresentation")]
+		string SimpleRepresentation ();
+
+		[Export ("intersectsWithEnvelope:")]
+		bool IntersectsWithEnvelope (AGSEnvelope envelope);
+
+		[Export ("intersectionWithEnvelope:")]
+		AGSEnvelope IntersectionWithEnvelope (AGSEnvelope envelope);
+
+		[Export ("isEmpty")]
+		bool IsEmpty ();
+
+		[Export ("isEqualToEnvelope:")]
+		bool IsEqualToEnvelope (AGSEnvelope other);
+	}
+
+	[BaseType (typeof (AGSGeometry))]
+	public partial interface AGSPoint {
+
+		[Export ("x")]
+		double X { get; }
+
+		[Export ("y")]
+		double Y { get; }
+
+		[Export ("initWithX:y:spatialReference:")]
+		IntPtr Constructor (double x, double y, AGSSpatialReference spatialReference);
+
+		[Static, Export ("pointWithX:y:spatialReference:")]
+		NSObject PointWithX (double x, double y, AGSSpatialReference spatialReference);
+
+		[Export ("isEqualToPoint:")]
+		bool IsEqualToPoint (AGSPoint other);
+
+		[Export ("distanceToPoint:")]
+		double DistanceToPoint (AGSPoint other);
+	}
+
+	[Model, BaseType (typeof (NSObject))]
+	public partial interface AGSLayerDelegate {
+
+		[Export ("layerDidLoad:")]
+		void LayerDidLoad (AGSLayer layer);
+
+		[Export ("layer:didFailToLoadWithError:")]
+		void DidFailToLoadWithError (AGSLayer layer, NSError error);
+
+		[Export ("layer:didInitializeSpatialReferenceStatus:")]
+		void DidInitializeSpatialReferenceStatus (AGSLayer layer, bool srStatusValid);
+	}
+
+	[BaseType (typeof (NSObject))]
+	public partial interface AGSLayer {
+
+		[Export ("loaded")]
+		bool Loaded { get; }
+
+		[Export ("error", ArgumentSemantic.Retain)]
+		NSError Error { get; }
+
+		[Export ("renderNativeResolution")]
+		bool RenderNativeResolution { get; set; }
+
+		[Export ("opacity")]
+		float Opacity { get; set; }
+
+		[Export ("name", ArgumentSemantic.Copy)]
+		string Name { get; set; }
+
+		[Export ("delegate", ArgumentSemantic.Assign)]
+		AGSLayerDelegate Delegate { get; set; }
+
+		[Export ("spatialReferenceStatusValid")]
+		bool SpatialReferenceStatusValid { get; set; }
+
+		[Export ("spatialReference", ArgumentSemantic.Retain)]
+		AGSSpatialReference SpatialReference { get; }
+
+		[Export ("fullEnvelope", ArgumentSemantic.Retain)]
+		AGSEnvelope FullEnvelope { get; }
+
+		[Export ("initialEnvelope", ArgumentSemantic.Retain)]
+		AGSEnvelope InitialEnvelope { get; set; }
+
+		[Export ("minScale")]
+		double MinScale { get; set; }
+
+		[Export ("maxScale")]
+		double MaxScale { get; set; }
+
+		[Export ("refresh")]
+		void Refresh ();
+
+		[Export ("timeAware")]
+		bool TimeAware { [Bind ("isTimeAware")]get; set;}
+
+		[Export ("visible")]
+		bool Visible { [Bind ("isVisible")]get; set; }
+
+		[Export ("isInScale")]
+		bool IsInScale ();
+
+	}
+	
+}
