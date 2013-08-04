16b9f0bb2e9bda83aff38ef357b8d7d1389001bc
beginor <beginor@qq.com>
2013-08-04 22:58:03 +0800

    try bind

--- Binding/libArcGIS.linkwith.cs
+++ Binding/libArcGIS.linkwith.cs
@@ -1,4 +1,10 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libArcGIS.a", LinkTarget.ArmV7 | LinkTarget.Simulator, IsCxx = true, ForceLoad = true)]
+[assembly: LinkWith ("libArcGIS.a",
+	LinkTarget.ArmV7 | LinkTarget.Simulator,
+	IsCxx = true,
+	ForceLoad = true,
+	Frameworks = "CoreGraphics CoreLocation CoreText Foundation QuartzCore MediaPlayer MobileCoreServices OpenGLES Security UIKit",
+	WeakFrameworks = "",
+   LinkerFlags = "-all_load -ObjC -F /Users/gdeic/ArcGIS -framework ArcGIS")]
