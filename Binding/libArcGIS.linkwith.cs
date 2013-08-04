e4ce20d493df5705ffa13cd8510b356eb2e6449c
beginor <beginor@qq.com>
2013-08-05 07:46:11 +0800

    works again with ios simulator!

--- Binding/libArcGIS.linkwith.cs
+++ Binding/libArcGIS.linkwith.cs
@@ -1,10 +1,11 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libArcGIS.a",
+[assembly: LinkWith (
+	"libArcGIS.a",
 	LinkTarget.ArmV7 | LinkTarget.Simulator,
 	IsCxx = true,
 	ForceLoad = true,
 	Frameworks = "CoreGraphics CoreLocation CoreText Foundation QuartzCore MediaPlayer MobileCoreServices OpenGLES Security UIKit",
-	WeakFrameworks = "",
-   LinkerFlags = "-all_load -ObjC -F /Users/gdeic/ArcGIS -framework ArcGIS")]
+	LinkerFlags = "-lz -lc++"
+	)]
