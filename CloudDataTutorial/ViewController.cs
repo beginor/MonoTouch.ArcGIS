c14ca35d54efe618e0db7d880bea360971ddaa0b
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2013-08-14 14:18:13 +0800

    cloud data tutorial step 1

--- CloudDataTutorial/ViewController.cs
+++ CloudDataTutorial/ViewController.cs
@@ -3,10 +3,12 @@ using System;
 using System.Drawing;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using MonoTouch.ArcGIS;
 
 namespace CloudDataTutorial {
 
 	public partial class ViewController : UIViewController {
+
 		public ViewController() : base ("ViewController", null) {
 		}
 
@@ -19,8 +21,22 @@ namespace CloudDataTutorial {
 
 		public override void ViewDidLoad() {
 			base.ViewDidLoad();
-			
-			// Perform any additional setup after loading the view, typically from a nib.
+			// add a basemap tiled layer.
+			var url = NSUrl.FromString("http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer");
+			var tiledLayer = AGSTiledMapServiceLayer.TiledMapServiceLayerWithURL(url);
+			this.MapView.AddMapLayer(tiledLayer, "Basemap Tiled Layer");
+
+			// cloud data
+			var featureLayerUrl = NSUrl.FromString("http://services.arcgis.com/oKgs2tbjK6zwTdvi/arcgis/rest/services/Major_World_Cities/FeatureServer/0");
+			var featureLayer = AGSFeatureLayer.FeatureServiceLayerWithURL(featureLayerUrl, AGSFeatureLayerMode.OnDemand);
+			this.MapView.AddMapLayer(featureLayer, "CloudData");
+
+			// symbology
+			AGSSimpleMarkerSymbol featureSymbol = AGSSimpleMarkerSymbol.SimpleMarkerSymbolWithColor(UIColor.FromRGBA(0f, 0.46f, 0.68f, 1f));
+			featureSymbol.Size = new SizeF(7, 7);
+			featureSymbol.Style = AGSSimpleMarkerSymbolStyle.Circle;
+			//featureSymbol.Outline
+			featureLayer.Renderer = AGSSimpleRenderer.SimpleRendererWithSymbol(featureSymbol);
 		}
 	}
 }
--- CloudDataTutorial/ViewController.designer.cs
+++ CloudDataTutorial/ViewController.designer.cs
@@ -1,18 +1,26 @@
 // WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
 
 namespace CloudDataTutorial
 {
 	[Register ("ViewController")]
 	partial class ViewController
 	{
+		[Outlet]
+		MonoTouch.ArcGIS.AGSMapView MapView { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (MapView != null) {
+				MapView.Dispose ();
+				MapView = null;
+			}
 		}
 	}
 }
